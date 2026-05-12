1d12915b1ab33e67f2ad37594ceea2eb75f101b4
sekiguchi <your_email@example.com>
2026-05-12 15:07:31 +0900

    fix/config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     REDIS_HOST: str
+    REDIS_PORT: str
     CELERY_BROKER_URL: str
     CELERY_RESULT_BACKEND: str
     SECRET_KEY: str
